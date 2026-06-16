9a148a804daab0a6fd2627eec8b86f858a783e38
priyanshistarbyte-sys <priyanshi.starbyte@gmail.com>
2026-06-16 12:07:13 +0530

    16-06-26

--- archive/games/halloween-witch/scripts/c3main.js
+++
@@ -1,6 +0,0 @@
-import "./../xmlDom.js";
-import "./../xpath.js";
-import "./c3runtime.js";
-import "./objRefTable.js";
-import "./project/main.js";
-import "./project/javaScriptInEvents.js";
--- archive/games/puppy-jump/scripts/c3main.js
+++
@@ -1,6 +0,0 @@
-import "./../xmldom.js";
-import "./../xpath.js";
-import "./c3runtime.js";
-import "./objRefTable.js";
-import "./project/main.js";
-import "./project/javaScriptInEvents.js";
